0204e8803cbf71683339c09de426048743f4ac74
Tharushika-Ekanayaka <tharushikamekanayaka@gmail.com>
2025-01-02 18:04:40 +0530

    createProduction_plan

---
+++ SCM_System/SCM_System/src/main/java/com/Project/SCM_System/Model/Production_Plan.java
@@ -0,0 +1,52 @@
+package com.Project.SCM_System.Model;
+
+@Document(collection = "Production_Plan")
+public class Production_Plan {
+    @Id
+    private String PlanId;
+    private String TargetQuantity;
+    private String StartDate;
+    private String EndDate;
+    private String ProductId;
+
+    public String getPlanId() {
+        return PlanId;
+    }
+
+    public void setPlanId(String PlanId) {
+        this.PlanId = PlanId;
+    }
+
+    public String getTargetQuantity() {
+        return TargetQuantity;
+    }
+
+    public void setTargetQuantity(String TargetQuantity) {
+        this.TargetQuantity = TargetQuantity;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getEndDate() {
+        return EndDate;
+    }
+
+    public void setEndDate(String EndDate) {
+        this.EndDate = EndDate;
+
+    }
+
+    public String getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(String ProductId) {
+        this.ProductId = ProductId;
+    }
+}
