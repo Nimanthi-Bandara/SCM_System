e0ce8d21c5514479a70cd411269af35d5967a5a5
Tharushika-Ekanayaka <tharushikamekanayaka@gmail.com>
2025-01-17 12:00:19 +0530

    14th commit

--- SCM_System/SCM_System/src/main/java/com/Project/SCM_System/Controller/Production_PlanController.java
+++ SCM_System/SCM_System/src/main/java/com/Project/SCM_System/Controller/Production_PlanController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Project.SCM_System.Model.Production_Plan;
+import com.Project.SCM_System.Model.Supplier;
 
 @RestController
 @RequestMapping({ "/api/Production_Plan" })
@@ -25,15 +26,15 @@ public class Production_PlanController {
     }
 
     @PostMapping
-    public ResponseEntity<ResponseEntity<ResponseEntity<ResponseEntity<Production_Plan>>>> createProduction_Plan(
+    public ResponseEntity<ResponseEntity<ResponseEntity<Production_Plan>>> createProduction_Plan(
             @RequestBody Production_Plan production_plan) {
         ResponseEntity<ResponseEntity<ResponseEntity<Production_Plan>>> creatProduction_Plan = this.Production_PlanService
                 .createProduction_Plan(production_plan);
-        return ResponseEntity.ok(creatProduction_Plan);
+        return ResponseEntity.ok(createProduction_Plan);
     }
 
     @GetMapping({ "/{OrderId}" })
-    public ResponseEntity<Production_Plan> getSupplierById(@PathVariable String suppId) {
+    public ResponseEntity<Production_Plan> getProduction_PlanById(@PathVariable String suppId) {
         Production_Plan production_plan = this.Production_PlanService.getProduction_PlanById(OrderId);
         return production_plan != null ? ResponseEntity.ok(production_plan) : ResponseEntity.notFound().build();
     }
@@ -45,19 +46,18 @@ public class Production_PlanController {
         return ResponseEntity.ok(production_plan);
     }
 
-    @PutMapping({ "/OrderId" })
+    @PutMapping({ "/{OrderId}" })
     public ResponseEntity<Production_Plan> updateProduction_Plan(@PathVariable String OrderId,
             @RequestBody Production_Plan production_plan) {
-        Production_Plan updateProduction_Plan = this.Production_PlanService.updateProduction_Plan(OrderId,
-                production_plan);
-        return updateProduction_Plan(OrderId, production_plan) != null
-                ? ResponseEntity.ok(updateProduction_Plan(OrderId, production_plan))
+        ResponseEntity<Production_Plan> updateProduction_Plan = this.Production_PlanService
+                .updateProduction_Plan(OrderId, production_plan);
+        return updateProduction_Plan != null ? ResponseEntity.ok(updateProduction_Plan)
                 : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping({ "/{OrderId}" })
     public ResponseEntity<Production_Plan> deleteProduction_Plan(@PathVariable String suppId) {
-        this.Production_PlanService.deleteProduction_Plan(OrderId)(OrderId);
+        this.Production_PlanService.deleteProduction_Plan(OrderId);
         return ResponseEntity.noContent().build();
     }
 }
