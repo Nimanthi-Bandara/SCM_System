fd392461b88892994b1c409f4a87d40e4e52fdc8
Tharushika-Ekanayaka <tharushikamekanayaka@gmail.com>
2025-01-17 12:06:49 +0530

    Production_Plan_Repository

---
+++ SCM_System/SCM_System/src/main/java/com/Project/SCM_System/Repository/Production_PlanRepository.java
@@ -0,0 +1,9 @@
+package com.Project.SCM_System.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.Project.SCM_System.Model.Production_Plan;
+
+public interface production_PlanRepository extends MongoRepository<Production_Plan, String> {
+
+}
